65682d9cea4b98424c7c8adf6ee0e5c5773efa66
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-15 23:21:52 +0900

    feat: 구분자 유효성 검증 기능
    커스텀 구분자가 기본 구분자와 같거나 숫자이면 IllegalArgumentException이 발생.

--- src/main/java/calculator/CalculatorValidator.java
+++ src/main/java/calculator/CalculatorValidator.java
@@ -1,13 +1,41 @@
 package calculator;
 
 public class CalculatorValidator {
-    private static final String SEPARATOR_START = "//";
-    private static final String SEPARATOR_END = "\\n";
+    private static final String CUSTOM_SEPARATOR_START = "//";
+    private static final String CUSTOM_SEPARATOR_END = "\\n";
+    private static final String BASIC_SEPARATOR_1 =",";
+    private static final String BASIC_SEPARATOR_2 =":";
 
     public boolean hasCustomSeparator(String input){
-        if(input.startsWith(SEPARATOR_START) && input.indexOf(SEPARATOR_END) >0){
+        if(input.startsWith(CUSTOM_SEPARATOR_START) && input.indexOf(CUSTOM_SEPARATOR_END) >0){
             return  true;
         }
         return false;
+    }
+
+    public boolean validateCustomSeparator(String separator){
+        if(isBasicSeparator(separator))
+            throw new IllegalArgumentException("신규 커스텀 구분자를 추가해 주세요");
+
+        if(isNumber(separator))
+            throw new IllegalArgumentException("구분자는 숫자일수 없습니다.");
+
+        return true;
+    }
+
+    private boolean isBasicSeparator(String separator){
+        if(separator.equals(BASIC_SEPARATOR_1) || separator.equals(BASIC_SEPARATOR_2)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isNumber(String input){
+        try{
+            Integer.parseInt(input);
+            return true;
+        }catch (NumberFormatException ex){
+            return false;
+        }
     }
 }
