c42a9602bfc7f852b25f1d7a38860408b1c58e00
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-16 23:07:14 +0900

    feat: 결과 표시 기능 추가
    OutputView 클래스 추가.
    객체 생성 안하고 바로 사용 가능하게 static 으로 설정.

---
+++ src/main/java/calculator/OutputView.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class OutputView {
+    private static final String RESULT = "결과 : ";
+
+    public static void outputResult(int result) {
+        System.out.println(RESULT + result);
+    }
+}
