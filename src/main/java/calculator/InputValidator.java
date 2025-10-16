1f9de83cfedff0114cc6f5e2bd2c436422fff201
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-16 23:59:13 +0900

    style: 줄정렬

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -5,39 +5,41 @@ public class InputValidator {
 
     public static final String CUSTOM_SEPARATOR_START = "//";
     public static final String CUSTOM_SEPARATOR_END = "\\n";
-    public static final String BASIC_SEPARATOR_1 =",";
-    public static final String BASIC_SEPARATOR_2 =":";
+    public static final String BASIC_SEPARATOR_1 = ",";
+    public static final String BASIC_SEPARATOR_2 = ":";
 
-    private InputValidator(){}
+    private InputValidator() {
+    }
 
-    public static InputValidator getInstance (){
-        if(inputValidator == null) inputValidator = new InputValidator();
+    public static InputValidator getInstance() {
+        if (inputValidator == null) inputValidator = new InputValidator();
         return inputValidator;
     }
 
-    public boolean hasCustomSeparator(String input){
-        if(input.startsWith(CUSTOM_SEPARATOR_START) && input.indexOf(CUSTOM_SEPARATOR_END) >0){
-            return  true;
+    public boolean hasCustomSeparator(String input) {
+        if (input.startsWith(CUSTOM_SEPARATOR_START) && input.indexOf(CUSTOM_SEPARATOR_END) > 0) {
+            return true;
         }
         return false;
     }
 
-    public boolean validateCustomSeparator(String separator){
-        if(isBasicSeparator(separator))
+    public boolean validateCustomSeparator(String separator) {
+        if (isBasicSeparator(separator))
             throw new IllegalArgumentException("신규 커스텀 구분자를 추가해 주세요");
 
         // 구분자에 숫자를 포함시키면 실제 숫자와 구분자의 경계가 모호해짐
-        if(hasNumber(separator))
+        if (hasNumber(separator))
             throw new IllegalArgumentException("구분자에는 숫자가 포함될 수 없습니다.");
 
         return true;
     }
 
-    public boolean validateInputNumber(String element){
-        if(!isNumber(element))
+    public boolean validateInputNumber(String element) {
+
+        if (!isNumber(element))
             throw new IllegalArgumentException("더하려는 값이 숫자가 아닙니다.");
 
-        if(Integer.parseInt(element) <0)
+        if (Integer.parseInt(element) < 0)
             throw new IllegalArgumentException("양수만 입력받을 수 있습니다.");
 
         return true;
@@ -58,12 +60,12 @@ public class InputValidator {
         }
     }
 
-    private boolean hasNumber(String input){
+    private boolean hasNumber(String input) {
         return input.matches(".*\\d.*");
     }
 
-    private boolean isBasicSeparator(String separator){
-        if(separator.equals(BASIC_SEPARATOR_1) || separator.equals(BASIC_SEPARATOR_2)){
+    private boolean isBasicSeparator(String separator) {
+        if (separator.equals(BASIC_SEPARATOR_1) || separator.equals(BASIC_SEPARATOR_2)) {
             return true;
         }
         return false;
