76543c2604d7d6dfde415edeb2bceab078ef5723
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-17 00:04:20 +0900

    feat: 문자열 프로그램이 실행되도록 완성
    기존에 만든 함수 조합해서 실행되도록 완성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculator calculator = Calculator.getInstance();
+        int result = calculator.addInputNums(InputView.inputWords());
+        OutputView.outputResult(result);
     }
 }
