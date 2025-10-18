a052056bae51ebc9e73512e83556a84284cebb37
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2025-10-18 20:08:47 +0900

    test: 프로그램 정상값 입력 테스트
    정상적인 값 테스트 코드 추가.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -2,6 +2,9 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,10 +20,32 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 공백값_입력_테스트() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'//;\\n', '0'",
+            "'1,2:3', '6'",
+            "'//;\\n', '0'",
+            "'//a\\n1a2:3a4', '10'"
+    })
+    void 정상값_입력값_테스트(String input, String output) {
+        assertSimpleTest(() -> {
+            run(input);
+            assertThat(output()).contains("결과 : " + output);
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
